804ba024b7a897c2a81093f8b4c413f855893318
madhushimadhuni24-ops <madhushimadhuni24@gmail.com>
2026-05-10 11:47:27 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1778393523259_yw9m6hayw*```",
 PORT: process.env.PORT || "8000"
 };
